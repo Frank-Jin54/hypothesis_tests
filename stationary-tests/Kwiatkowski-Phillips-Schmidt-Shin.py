4e4a23b194fb644b3951a8a1f2e6efbff292619f
Frank Jin <fjin@chata.ai>
2022-02-05 11:02:22 -0700

    add test

---
+++ stationary-tests/Kwiatkowski-Phillips-Schmidt-Shin.py
@@ -0,0 +1,22 @@
+"""
+Tests whether a time series is trend stationary or not.
+
+Assumptions
+
+Observations in are temporally ordered.
+Interpretation
+
+H0: the time series is trend-stationary.
+H1: the time series is not trend-stationary.
+"""
+
+
+# Example of the Kwiatkowski-Phillips-Schmidt-Shin test
+from statsmodels.tsa.stattools import kpss
+data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+stat, p, lags, crit = kpss(data)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably Stationary')
+else:
+	print('Probably not Stationary')
